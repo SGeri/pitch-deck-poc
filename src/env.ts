9a14298212677236fbb93c0137ddaf87d9b4d908
SGeri <sarffygeri@gmail.com>
2026-02-02 12:13:38 +0100

    feat: unused stuff

--- src/env.ts
+++ src/env.ts
@@ -9,13 +9,6 @@ export const env = createEnv({
 
         BETTER_AUTH_SECRET: z.string().min(1),
         BETTER_AUTH_URL: z.string().min(1),
-
-        AWS_REGION: z.string().optional(),
-        AWS_ACCESS_KEY_ID: z.string().optional(),
-        AWS_SECRET_ACCESS_KEY: z.string().optional(),
-        S3_ENDPOINT: z.string().optional(),
-        S3_BUCKET_NAME: z.string().optional(),
-        S3_FORCE_PATH_STYLE: z.string().optional(),
     },
     client: {},
     runtimeEnv: {
@@ -23,11 +16,5 @@ export const env = createEnv({
         OPENAI_API_KEY: process.env.OPENAI_API_KEY,
         BETTER_AUTH_SECRET: process.env.BETTER_AUTH_SECRET,
         BETTER_AUTH_URL: process.env.BETTER_AUTH_URL,
-        AWS_REGION: process.env.AWS_REGION,
-        AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
-        AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
-        S3_ENDPOINT: process.env.S3_ENDPOINT,
-        S3_BUCKET_NAME: process.env.S3_BUCKET_NAME,
-        S3_FORCE_PATH_STYLE: process.env.S3_FORCE_PATH_STYLE,
     },
 });
